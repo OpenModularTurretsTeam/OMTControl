2b039e55e30da3d2cfa66c68abbf8c781543cf40
rokiyo <rokiyo@gmail.com>
2017-05-21 23:15:23 +1000

    1st attempt at continuous fire while right clicking

--- src/main/java/omtteam/omtcontrol/items/ItemLaserPointer.java
+++ src/main/java/omtteam/omtcontrol/items/ItemLaserPointer.java
@@ -1,5 +1,6 @@
 package omtteam.omtcontrol.items;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -44,11 +45,26 @@ public class ItemLaserPointer extends CompatItem {
     }
 
     @Override
+    public void onUpdate(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
+        super.onUpdate(stack, worldIn, entityIn, itemSlot, isSelected);
+        NBTTagCompound tagCompound = getTagCompound(stack);
+
+        if (isSelected && entityIn instanceof EntityPlayer && tagCompound.getBoolean("isActive")) {
+            aimAllTurrets(worldIn, (EntityPlayer) entityIn, stack);
+
+            if (worldIn.getTotalWorldTime() > tagCompound.getLong("lastActivated") + 5) {
+                //stop firing if more than 5 ticks have passed since the player last right-clicked.
+                setAllTurretsForceFire(worldIn, stack, false);
+            }
+        }
+    }
+
+    @Override
     @Nonnull
     public EnumActionResult clOnItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
         if (player.isSneaking()) {
             ItemStack stack = player.getHeldItem(hand);
-            clearLinkedBases(stack);
+            clearNBTData(stack);
         }
         return super.clOnItemUse(player, world, pos, hand, facing, hitX, hitY, hitZ);
     }
@@ -57,46 +73,79 @@ public class ItemLaserPointer extends CompatItem {
     protected ActionResult<ItemStack> clOnItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand hand) {
         ItemStack itemStackIn = playerIn.getHeldItem(hand);
         if (playerIn.isSneaking()) {
-            clearLinkedBases(itemStackIn);
+            clearNBTData(itemStackIn);
         } else {
             if(hasLinkedBases(itemStackIn)) {
-                NBTTagList linkedBases = getLinkedBases(itemStackIn);
-
-                for (int i = 0; i < linkedBases.tagCount(); i++) {
-                    int[] blockPosArray = linkedBases.getCompoundTagAt(i).getIntArray("blockPos");
-                    BlockPos pos = new BlockPos(blockPosArray[0], blockPosArray[1], blockPosArray[2]);
-
-                    TileEntityBaseAddonMain addon = (TileEntityBaseAddonMain) worldIn.getTileEntity(pos);
-                    if (addon != null) {
-                        addon.setAllTurretsYawPitch(playerIn.rotationYaw, playerIn.rotationPitch);
-
-                        //TODO: Use the tile entity to force-fire the turrets for a few ticks, so that turret's fire rate is not limited by item's use speed.
-                        addon.forceShootAllTurrets();
-                    }
-                }
+                //TODO: Look into a method that doesn't constantly overwrite the item's NBT data... It makes the item constantly refresh in the player's hand.
+                setLastActivation(worldIn, playerIn);
+                setAllTurretsForceFire(worldIn, itemStackIn, true);
             }
-
         }
         return super.clOnItemRightClick(worldIn, playerIn, hand);
     }
 
     @Nonnull
+    public void aimAllTurrets(World worldIn, EntityPlayer playerIn, ItemStack itemStackIn) {
+        if(hasLinkedBases(itemStackIn)) {
+            NBTTagList linkedBases = getLinkedBases(itemStackIn);
+
+            for (int i = 0; i < linkedBases.tagCount(); i++) {
+                int[] blockPosArray = linkedBases.getCompoundTagAt(i).getIntArray("blockPos");
+                BlockPos pos = new BlockPos(blockPosArray[0], blockPosArray[1], blockPosArray[2]);
+
+                TileEntityBaseAddonMain addon = (TileEntityBaseAddonMain) worldIn.getTileEntity(pos);
+                if (addon != null) {
+                    addon.setAllTurretsYawPitch(playerIn.rotationYaw, playerIn.rotationPitch);
+                }
+            }
+        }
+    }
+
+    @Nonnull
+    public void setAllTurretsForceFire(World worldIn, ItemStack itemStackIn, boolean state) {
+        if(hasLinkedBases(itemStackIn)) {
+            NBTTagList linkedBases = getLinkedBases(itemStackIn);
+            NBTTagCompound tagCompound = getTagCompound(itemStackIn);
+            tagCompound.setBoolean("isActive", state);
+
+            for (int i = 0; i < linkedBases.tagCount(); i++) {
+                int[] blockPosArray = linkedBases.getCompoundTagAt(i).getIntArray("blockPos");
+                BlockPos pos = new BlockPos(blockPosArray[0], blockPosArray[1], blockPosArray[2]);
+
+                TileEntityBaseAddonMain addon = (TileEntityBaseAddonMain) worldIn.getTileEntity(pos);
+                if (addon != null) {
+                    addon.setAllTurretsForceFire(state);
+
+                    //TODO: Remove the following line once auto-firing is working correctly.
+                    if(state) { addon.forceShootAllTurrets(); }
+                }
+            }
+        }
+    }
+
+    @Nonnull
+    public NBTTagCompound getTagCompound(ItemStack stack) {
+        if (!stack.hasTagCompound()) {
+            stack.setTagCompound(new NBTTagCompound());
+        }
+        return stack.getTagCompound();
+    }
+
+    @Nonnull
     public boolean hasLinkedBases(ItemStack stack) {
-        return stack.hasTagCompound() && stack.getTagCompound().hasKey("turretBases");
+        return getTagCompound(stack).hasKey("turretBases");
     }
 
     @Nonnull
     public NBTTagList getLinkedBases(ItemStack stack) {
-        return stack.hasTagCompound() ? stack.getTagCompound().getTagList("turretBases", Constants.NBT.TAG_COMPOUND) : new NBTTagList();
+        return getTagCompound(stack).getTagList("turretBases", Constants.NBT.TAG_COMPOUND);
     }
 
-    public void clearLinkedBases(ItemStack stack) {
-        if (stack.hasTagCompound()) {
-            Set<String> keySet = stack.getTagCompound().getKeySet();
+    public void clearNBTData(ItemStack stack) {
+        Set<String> keySet = getTagCompound(stack).getKeySet();
 
-            if (keySet != null) {
-                keySet.clear();
-            }
+        if (keySet != null) {
+            keySet.clear();
         }
     }
 
@@ -110,21 +159,26 @@ public class ItemLaserPointer extends CompatItem {
         newBase.setIntArray("blockPos", blockPosArray);
         linkedBases.appendTag(newBase);
 
-        if (stack.hasTagCompound()) {
-            stack.getTagCompound().setTag("turretBases", linkedBases);
-        } else {
-            NBTTagCompound tagCompound = new NBTTagCompound();
-            tagCompound.setTag("turretBases", linkedBases);
-            stack.setTagCompound(tagCompound);
-        }
+        getTagCompound(stack).setTag("turretBases", linkedBases);
+    }
+
+    public void setLastActivation(World worldIn, EntityPlayer playerIn) {
+        ItemStack stack = playerIn.getHeldItemMainhand();
+        NBTTagCompound tagCompound = getTagCompound(stack);
+
+        tagCompound.setLong("lastActivated", worldIn.getTotalWorldTime());
+        tagCompound.setBoolean("isActive", true);
     }
 
     @SideOnly(Side.CLIENT)
     @SuppressWarnings("unchecked")
     public void addInformation(ItemStack stack, EntityPlayer player, List tooltip, boolean isAdvanced) {
         if(hasLinkedBases(stack)) {
+            NBTTagCompound tagCompound = getTagCompound(stack);
             NBTTagList linkedBases = getLinkedBases(stack);
             tooltip.add("\u00A76Linked Bases: \u00A7b" + linkedBases.tagCount());
+            tooltip.add("\u00A76Active: \u00A7b" + tagCompound.getBoolean("isActive"));
+            tooltip.add("\u00A76LastUsed: \u00A7b" + tagCompound.getLong("lastActivated"));
         }
     }
 }
