2c2bb33f0e15f06966d48b2de89c92212ce3d491
Keridos <2742845+Keridos@users.noreply.github.com>
2017-05-24 16:09:35 +0200

    replacing a bit of code in build.gradle to make version deps work

--- src/main/java/omtteam/omtcontrol/reference/Reference.java
+++ src/main/java/omtteam/omtcontrol/reference/Reference.java
@@ -4,9 +4,8 @@ public class Reference {
     public static final String MOD_ID = "omtcontrol";
     public static final String NAME = "Open Modular Turrets Control";
     public static final String VERSION = "@VERSION@";
-    private static final String OMLIB_MIN_VERSION = "3.0.0-91";
-    private static final String OMT_MIN_VERSION = "3.0.0-149";
     public static final String DEPENDENCIES = "after:ThermalFoundation;after:ThermalExpansion;after:OpenComputers;" +
             "after:ComputerCraft;after:Mekanism;after:EnderIO;after:Thaumcraft;" +
-            "required-after:omlib@" + OMLIB_MIN_VERSION + ";required-after:openmodularturrets@" + OMT_MIN_VERSION;
+            "required-after:omlib@[0.0.0,);" +
+            "required-after:openmodularturrets@[0.0.0,)";
 }
