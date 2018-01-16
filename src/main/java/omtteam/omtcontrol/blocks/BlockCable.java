2a92fa9a228e269c69b83a92e651306af1a0697e
dmf444 <becat444@gmail.com>
2018-01-16 06:45:10 -0500

    Some minor work on cables
    I'm changing PC's need to push code up, this is far from finished

--- src/main/java/omtteam/omtcontrol/blocks/BlockCable.java
+++ src/main/java/omtteam/omtcontrol/blocks/BlockCable.java
@@ -1,5 +1,41 @@
 package omtteam.omtcontrol.blocks;
 
 
-public class BlockCable {
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import omtteam.omlib.blocks.BlockAbstractTileEntity;
+import omtteam.omtcontrol.tileentity.TileEntityBasicCable;
+import omtteam.openmodularturrets.api.network.INetworkCable;
+import omtteam.openmodularturrets.api.network.OMTNetwork;
+
+import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BlockCable extends BlockAbstractTileEntity{
+
+
+    protected BlockCable() {
+        super(Material.GLASS);
+    }
+
+    @Nonnull
+    @Override
+    public TileEntity createTileEntity(World world, IBlockState state) {
+        return new TileEntityBasicCable();
+    }
+
+    public void onBlockPlacedBy(World world, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
+        TileEntity tile = world.getTileEntity(pos);
+        if(tile instanceof TileEntityBasicCable) {
+            TileEntityBasicCable cable = (TileEntityBasicCable) tile;
+            cable.createNetwork();
+        }
+    }
 }
